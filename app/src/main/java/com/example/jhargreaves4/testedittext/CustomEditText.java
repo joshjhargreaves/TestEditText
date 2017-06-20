d76454d84887b7567c490dcf5e1eb6fa258c122d
Josh Hargreaves <joshyhargreaves@gmail.com>
2017-06-20 23:56:47 +0100

    Added text watcher

--- app/src/main/java/com/example/jhargreaves4/testedittext/CustomEditText.java
+++ app/src/main/java/com/example/jhargreaves4/testedittext/CustomEditText.java
@@ -1,6 +1,8 @@
 package com.example.jhargreaves4.testedittext;
 
 import android.content.Context;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputConnection;
@@ -8,16 +10,29 @@ import android.view.inputmethod.InputConnectionWrapper;
 import android.widget.EditText;
 
 public class CustomEditText extends EditText {
-    public CustomEditText(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-    }
 
     public CustomEditText(Context context, AttributeSet attrs) {
         super(context, attrs);
+        addInternalTextWatcher();
     }
 
-    public CustomEditText(Context context) {
-        super(context);
+    private void addInternalTextWatcher() {
+
+        super.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                System.out.println("");
+            }
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                System.out.println("");
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+                System.out.println("");
+            }
+        });
     }
 
     @Override
@@ -36,8 +51,11 @@ public class CustomEditText extends EditText {
         @Override
         public boolean deleteSurroundingText(int beforeLength, int afterLength) {
             // deleteSurroundingText(1, 0) will be called for backspace for 'committed' text
-            // We want to catch the case whereby TextWatcher won't work. I.E. an empty textbox
-            if (beforeLength == 1 && afterLength == 0 && CustomEditText.this.getText().length() == 0)
+            // We want to catch the case whereby TextWatcher won't work. I.E. an empty textbox,
+            // or cursor at is at the beginning of EditText and no text deleted.
+            // The rest of the cases we will deal with with TextWatcher
+            System.out.println("Selection: " + CustomEditText.this.getSelectionStart());
+            if (beforeLength == 1 && afterLength == 0 && CustomEditText.this.getSelectionStart() == 0)
             {
                 System.out.println("Delete on empty EditText");
             }
