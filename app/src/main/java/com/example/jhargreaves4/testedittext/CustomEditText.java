a94103fb282e578dbd6f69f2a770a28e195ed62a
Josh Hargreaves <jhargreaves4@bloomberg.net>
2017-06-20 19:34:34 +0100

    Add deletion detection

--- app/src/main/java/com/example/jhargreaves4/testedittext/CustomEditText.java
+++ app/src/main/java/com/example/jhargreaves4/testedittext/CustomEditText.java
@@ -2,15 +2,10 @@ package com.example.jhargreaves4.testedittext;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.view.KeyEvent;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputConnection;
 import android.view.inputmethod.InputConnectionWrapper;
 import android.widget.EditText;
-
-/**
- * Created by jhargreaves4 on 6/20/17.
- */
 
 public class CustomEditText extends EditText {
     public CustomEditText(Context context, AttributeSet attrs, int defStyle) {
@@ -27,38 +22,28 @@ public class CustomEditText extends EditText {
 
     @Override
     public InputConnection onCreateInputConnection(EditorInfo outAttrs) {
-        return new ZanyInputConnection(super.onCreateInputConnection(outAttrs),
+        return new InternalInputConnection(super.onCreateInputConnection(outAttrs),
                 true);
     }
 
-    private class ZanyInputConnection extends InputConnectionWrapper {
+    private class InternalInputConnection extends InputConnectionWrapper {
 
-        public ZanyInputConnection(InputConnection target, boolean mutable) {
+        public InternalInputConnection(InputConnection target, boolean mutable) {
             super(target, mutable);
         }
 
-        @Override
-        public boolean sendKeyEvent(KeyEvent event) {
-            if (event.getAction() == KeyEvent.ACTION_DOWN
-                    && event.getKeyCode() == KeyEvent.KEYCODE_DEL) {
-                // Un-comment if you wish to cancel the backspace:
-                // return false;
-            }
-            return super.sendKeyEvent(event);
-        }
 
         @Override
         public boolean deleteSurroundingText(int beforeLength, int afterLength) {
-            // magic: in latest Android, deleteSurroundingText(1, 0) will be called for backspace
-            if (beforeLength == 1 && afterLength == 0) {
-                // backspace
-                return sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DEL))
-                        && sendKeyEvent(new KeyEvent(KeyEvent.ACTION_UP, KeyEvent.KEYCODE_DEL));
+            // deleteSurroundingText(1, 0) will be called for backspace for 'committed' text
+            // We want to catch the case whereby TextWatcher won't work. I.E. an empty textbox
+            if (beforeLength == 1 && afterLength == 0 && CustomEditText.this.getText().length() == 0)
+            {
+                System.out.println("Delete on empty EditText");
             }
 
             return super.deleteSurroundingText(beforeLength, afterLength);
         }
-
     }
 
 }
